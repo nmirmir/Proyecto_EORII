03396030b489152d8b33b4e34d585b562c145a04
LordAthetos <132953919+FrancescAguera@users.noreply.github.com>
2024-12-03 16:43:15 +0100

    Server Hora
    Actualizado server hora con multiplicador de tiempo, cambiado fecha y hora por separado por un unico tipo datetime

---
+++ Server_hora.py
@@ -0,0 +1,64 @@
+import time
+from asyncua.sync import Server
+from asyncua import ua
+import json
+import datetime
+
+multiplicador = 1
+
+class Server_hora:
+    def __init__(self):
+        self.server = Server()
+        self.server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
+        self.server.set_server_name("OPC UA Simulation Server")
+        self.server.set_endpoint("opc.tcp://LAPTOP-PIE5PVF8:53530/OPCUA/SimulationServer") #IP Jaime
+        self.server.set_security_IDs(["Anonymous"])
+        self.uri = "http://www.epsa.upv.es/entornos/NJFJ"
+        self.idx = self.server.register_namespace(self.uri)
+        print(f'nuestro idx: {self.idx}')
+        time.sleep(1)
+        self.timestamps = self.process_json_timestamps("poyo.json")
+        
+
+    def run(self):
+        self.server.start()
+        print("Server started successfully!")
+        self.fechaHora = self.setupVariables()
+        self.server_running = True
+
+        for timestamp in self.timestamps:
+            if self.server_running:
+                self.send_data(timestamp)
+                time.sleep(300 / multiplicador)
+            else:
+                break   
+
+    def process_json_timestamps(self,json_file_path):
+        # Read JSON file
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+        
+        # Convert each date+time combination to datetime objects
+        timestamps = []
+        for reading in data:
+            date_str = reading['fecha']
+            time_str = reading['hora']
+            # Combine date and time strings and convert to datetime
+            datetime_str = f"{date_str} {time_str}"
+            timestamp = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+            timestamps.append(timestamp)        
+        return timestamps
+    
+    def setupVariables(self):
+        objHorario = self.server.nodes.objects.add_object(self.idx, "Objeto_Horario")
+        fechaHora = objHorario.add_variable(self.idx, "FechaHora", self.timestamps[0])
+        fechaHora.set_writable()
+        return fechaHora
+
+    def send_data(self,timestamp):
+        self.fechaHora.write_value(timestamp)
+
+if __name__ == "__main__":
+    server = Server_hora()
+    server.run()
+
