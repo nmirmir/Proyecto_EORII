9d7cdf9116feab33de07cbc364356b880aff45e3
Nicolás Miró <nicolasmiromira@gmail.com>
2024-12-07 12:43:33 +0100

    estamos con el server de caudal, hay que  hacer que tenga la estructura de datos definida en el xml

--- Server_caudal.py
+++ Server_caudal.py
@@ -10,52 +10,110 @@ class Server_caudal:
         self.server = Server()
         self.server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
         self.server.set_server_name("OPC UA Simulation Server")
-        self.server.set_endpoint("opc.tcp://LAPTOP-QJA0AD04:53530/OPCUA/SimulationServer")
+        self.server.set_endpoint("opc.tcp://DESKTOP-M1F986I:5360/OPCUA/SimulationServer")
         self.server.set_security_IDs(["Anonymous"])
         self.uri = "http://www.epsa.upv.es/entornos/NJFJ/Caudal"
         self.idx = self.server.register_namespace(self.uri)
         print(f'nuestro idx: {self.idx}')
         time.sleep(1)
+        self.timestamps = self.process_json_data("poyo.json")
 
         # Add objects folder
         self.objects = self.server.nodes.objects
         
         # Create device folder
         self.devices_folder = self.objects.add_folder(self.idx, "Devices")
-        
-    def add_device(self, device_element, ns):
-        """Add a device and its datapoints to the OPC UA server"""
-        device_id = device_element.get('id')
-        device_name = device_element.find('ns:Name', ns).text
-        
-        # Create device folder
-        device_folder = self.devices_folder.add_folder(self.idx, device_name)
-        
-        # Add device properties
-        device_folder.add_variable(self.idx, "Description", 
-                                device_element.find('ns:Description', ns).text)
-        device_folder.add_variable(self.idx, "Type", 
-                                device_element.find('ns:Type', ns).text)
-        
-        # Add datapoints
-        datapoints = device_element.find('ns:DataPoints', ns)
-        for datapoint in datapoints.findall('ns:DataPoint', ns):
-            dp_name = datapoint.find('ns:Name', ns).text
-            dp_value = datapoint.find('ns:Value', ns).text
-            dp_type = datapoint.find('ns:DataType', ns).text
-            
-            # Convert value based on datatype
-            if dp_type == "Float":
-                dp_value = float(dp_value)
-            elif dp_type == "Integer":
-                dp_value = int(dp_value)
-            
-            var = device_folder.add_variable(self.idx, dp_name, dp_value)
-            var.set_writable()
+
+
 
     def run(self):
+        
         self.server.start()
         print("Server started successfully!")
+        self.hora, self.caudal, self.estado = self.setupVariables()
+        self.server_running = True
+
+        for data in self.timestamps:
+            if self.server_running:
+                self.send_data(data)
+                time.sleep(1.0)  # Or whatever interval you want
+            else:
+                break
+    def process_json_data(self, json_file_path):
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+        
+        # We'll modify this to store both timestamps and aforo values
+        processed_data = []
+        for reading in data:
+            timestamp = datetime.datetime.strptime(reading['FechaHora'], '%Y-%m-%dT%H:%M:%S')
+            caudal_value = float(reading['Caudal (m3/s)'])  # Assuming this is the field name
+            processed_data.append({
+                'timestamp': timestamp,
+                'caudal': caudal_value
+            })
+        return processed_data
+
+    def setupVariables(self):
+        root = self.load_model()
+        ns = {'ns': 'http://opcfoundation.org/UA/2011/03/UANodeSet.xsd'}
+        
+        # Get Aforo device from XML
+        equipment = root.find('.//ns:Equipment', ns)
+        caudal_device = equipment.find(".//ns:Device[@id='S_Caudal']", ns)
+        
+        # Create folder structure
+        device_name = caudal_device.find('ns:Name', ns).text
+        folder_caudal = self.server.nodes.objects.add_folder(self.idx, device_name)
+        
+        # Create the object
+        device_description = caudal_device.find('ns:Description', ns).text
+        objCaudal = folder_caudal.add_object(self.idx, device_description)
+        
+        # Add variables based on XML DataPoints
+        datapoints = caudal_device.find('ns:DataPoints', ns)
+        
+        # Create Caudal variable
+        caudal_dp = datapoints.find(".//ns:DataPoint[@id='Caudal']", ns)
+        caudal = objCaudal.add_variable(
+            self.idx,
+            caudal_dp.find('ns:Name', ns).text,
+            float(caudal_dp.find('ns:Value', ns).text),
+            ua.VariantType.Float
+        )
+        
+        # Create Estado variable
+        estado_dp = datapoints.find(".//ns:DataPoint[@id='Caudal_Estado']", ns)
+        estado = objCaudal.add_variable(
+            self.idx,
+            estado_dp.find('ns:Name', ns).text,
+            estado_dp.find('ns:Value', ns).text.lower() == 'true',
+            ua.VariantType.Boolean
+        )
+        
+        # Create Hora variable
+        hora_dp = datapoints.find(".//ns:DataPoint[@id='Hora_Caudal']", ns)
+        hora = objCaudal.add_variable(
+            self.idx,
+            hora_dp.find('ns:Name', ns).text,
+            self.timestamps[0]['timestamp'],
+            ua.VariantType.DateTime
+        )
+        print(f"Hora: {hora}")
+        print(f"Caudal: {caudal}")
+        print(f"Estado: {estado}")
+        caudal.set_writable()
+        estado.set_writable()
+        hora.set_writable()
+        return hora, caudal, estado
+
+    
+
+    def send_data(self, data):
+        print(f"Timestamp: {data['timestamp']}, Caudal: {data['caudal']}")
+        self.hora.write_value(data['timestamp'])
+        self.caudal.write_value(ua.Float(data['caudal']))
+        # Estado could be updated based on some condition if needed
 
 def load_model():
     tree = ET.parse('Modelo_datos.xml')
@@ -73,10 +131,10 @@ if __name__ == "__main__":
         
         # Get Equipment node and Aforo device
         equipment = root.find('.//ns:Equipment', ns)
-        aforo_device = equipment.find(".//ns:Device[@id='S_Aforo']", ns)
+        caudal_device = equipment.find(".//ns:Device[@id='S_Caudal']", ns)
         
         # Add device to server
-        server.add_device(aforo_device, ns)
+        #server.add_device(caudal_device, ns)
         
         # Start the server
         server.run()
